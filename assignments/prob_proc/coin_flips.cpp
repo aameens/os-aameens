b2d89132a03998564a1348842fe6b075c0447595
Aidan Meens <aameens@mavs.coloradomesa.edu>
2024-11-13 12:42:35 -0700

    fix submit prob proc

--- assignments/prob_proc/coin_flips.cpp
+++ assignments/prob_proc/coin_flips.cpp
@@ -1,32 +1,3 @@
-/*
-#include <stdio.h>
-#include <unistd.h>
-#include <math.h>
-
-double probability = 1.0;
-double score = 100.0;
-
-int main()
-{
-    for (int i = 0; i < 3; ++i)
-    {
-        if (fork() == 0)
-        {
-            probability *= 0.1;
-            score *= 1.2;
-        }
-        else
-        {
-            probability *= 0.9;
-            score /= 1.11;    
-        }
-    }
-    printf("score = %lf with probability = %lf\n", score, probability);
-    
-    return 0;
-}
-*/
-
 #include <iostream>
 #include <unistd.h>
 #include <string>
@@ -52,6 +23,5 @@ int main()
         }
     }
 
-    // printf("Results: %s!, Probability: %lf\n", results, prob);
     cout << "Results: " << results << ", Probability: " << prob << endl;
 }
